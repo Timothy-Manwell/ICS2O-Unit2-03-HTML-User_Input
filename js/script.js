0ee4d9ffb0d047d3ef1c9eff52c8b68a94ae3995
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-12 14:26:47 -0400

    html and js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Timothy Manwell All rights reserved
+//
+// Created by: Timothy Manwell
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+ function enterClicked() {
+    // input
+    const streetNumber = parseInt(document.getElementById("street-number").value)
+    const streetName = document.getElementById("street-name").value
+    
+    // output
+    document.getElementById("address").innerHTML =
+      "Your adress is: " + streetNumber + streetName + "."
+  }
